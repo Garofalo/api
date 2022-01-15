e2dbdc36e8f7ca379c8aece22f8248fc5b47f72b
Rgarf <nothanks@gmail.com>
2022-01-15 18:14:42 -0500

    added random line

--- lib/app.py
+++ lib/app.py
@@ -1,11 +1,12 @@
 
-from distutils.log import debug
+from crypt import methods
 from peewee import *
 from playhouse.postgres_ext import *
 from flask import jsonify
 from flask import Flask
 from playhouse.shortcuts import model_to_dict, dict_to_model
 from playhouse.postgres_ext import *
+import random
 
 
 db = PostgresqlDatabase('poems', user='postgres',
@@ -62,6 +63,62 @@ def title(title=None):
         else:
             poems = jsonify(poems)
             return poems
+
+
+@app.route('/random/<author>', methods=['GET'])
+@app.route('/random/', methods=['GET'])
+def random_poem(author=None):
+
+    if author:
+        poems = []
+        for poem in Poem.select().where(Poem.author == author):
+            poem = model_to_dict(poem)
+            poems.append(poem)
+        if poems == []:
+            return jsonify({'Message': 'No Authors found. Remember to use full name and case sensitivity'})
+        else:
+            index = random.randint(1, len(poems))
+            poem = poems[index]
+            poem = jsonify(poem)
+            return poem
+    else:
+        poems = Poem.select()
+        index = random.randint(1, len(poems))
+        poem = poems[index]
+        poem = model_to_dict(poem)
+        poem = jsonify(poem)
+        return poem
+
+
+@app.route('/lines/<line>', methods=['GET'])
+@app.route('/lines/', methods=['GET'])
+def lines(line=None):
+    if line:
+        subquery = Poem.select(
+            Poem.lines,
+            fn.ARRAY_AGG(Poem.lines).alias('lines')).group_by(Poem.lines)
+        return 'Hi'
+        # for poem in Poem.select().where(Poem.lines == line):
+        #     poem = model_to_dict(poem)
+        #     poems.append(poem)
+        #     if poems == []:
+        #         return jsonify({"Message": 'No lines contain that sequence of letters.'})
+        #     else:
+        #         return jsonify(poems)
+
+    else:
+        poems = Poem.select()
+        index = random.randint(1, len(poems))
+        poem = poems[index]
+        poem = model_to_dict(poem)
+        lines = poem['lines']
+        rand_line_pos = random.randint(1, len(lines)-1)
+        rand_line = lines[rand_line_pos]
+        while rand_line == '':
+            rand_line_pos = random.randint(1, len(lines)-1)
+            rand_line = lines[rand_line_pos]
+        rand_line = jsonify(rand_line)
+        return rand_line
 
 
 app.run(port=9000, debug=True)
